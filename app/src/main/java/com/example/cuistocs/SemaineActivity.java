c032882f0e6dc669b3139cac7e9aa33d52f201f7
Robin Maxime <maxime.robin@student-cs.fr>
2020-11-16 17:49:44 +0100

    page d'accueil commit pre-pull

--- app/src/main/java/com/example/cuistocs/SemaineActivity.java
+++ app/src/main/java/com/example/cuistocs/SemaineActivity.java
@@ -54,6 +54,8 @@ public class SemaineActivity extends AppCompatActivity {
         versAvtivitéSemaine.putExtra("indiceSemaine", numeroSemaine);
         startActivity(versAvtivitéSemaine);
         */
+
+
     }
 
 }
