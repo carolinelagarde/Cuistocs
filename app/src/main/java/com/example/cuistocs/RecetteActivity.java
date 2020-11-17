2872282b3ec25d3a358ff51d0ec9fb89bd8c14a6
carolinelagarde <caro.lag@outlook.fr>
2020-11-17 15:37:10 +0100

    petites modifs pour pouvoir merge

--- app/src/main/java/com/example/cuistocs/RecetteActivity.java
+++ app/src/main/java/com/example/cuistocs/RecetteActivity.java
@@ -22,7 +22,7 @@ import java.util.Vector;
 public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     public int points;
-// checker pourquoi il reconnait pas la variable, et push car j'ai modifie l'erreur
+    // checker pourquoi il reconnait pas la variable, et push car j'ai modifie l'erreur
     Button boutonFini;
     Button boutonPasse;
     Recette recette;
@@ -49,7 +49,9 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
         SharedPreferences sp = getSharedPreferences("scoreActuel", Context.MODE_PRIVATE);
         if (sp.contains("nombrePoints")) {
             points = sp.getInt("nombrePoints", -1);
-        } else { points = 0; }
+        } else {
+            points = 0;
+        }
 
 
         // recuperation des entiers numeroSemaine et numeroJour qui servent à indexer les recettes dans la matrice des recettes
@@ -60,64 +62,62 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
 
         numeroJour = messagedeChoixJoursActivity.getIntExtra("numero jour", -1); //on recupere l entier nombre de point de l'activité principale
 
-        Intent messagedeSemaineActivity=getIntent();
-        numeroSemaine=messagedeSemaineActivity.getIntExtra("indiceSemaine",-1) ;
+        Intent messagedeSemaineActivity = getIntent();
+        numeroSemaine = messagedeSemaineActivity.getIntExtra("indiceSemaine", -1);
 
 
-        SeekBar barre=findViewById(R.id.seekBar); // barre de choix du nombre d'invités
+        SeekBar barre = findViewById(R.id.seekBar); // barre de choix du nombre d'invités
         barre.setOnSeekBarChangeListener(this);
 
         //////// accès à la matrice recette de test
-        Vector<Ingredient> lesIngredientsdelaRecette=new Vector<Ingredient>();
-        Vector <Recette> livreRecettes=new Vector<Recette>();
+        Vector<Ingredient> lesIngredientsdelaRecette = new Vector<Ingredient>();
+        Vector<Recette> livreRecettes = new Vector<Recette>();
 
-        lesIngredientsdelaRecette.add(new Ingredient(25,"g","mozarella"));
-        lesIngredientsdelaRecette.add(new Ingredient(4,"","oeufs"));
-        lesIngredientsdelaRecette.add(new Ingredient(1,"bouquet","ciboulette"));
-        lesIngredientsdelaRecette.add(new Ingredient(2,"cuilleres a soupe","huile"));
-        lesIngredientsdelaRecette.add(new Ingredient(1,"pincée","sel"));
-        lesIngredientsdelaRecette.add(new Ingredient(1,"pincée","poivre"));
+        lesIngredientsdelaRecette.add(new Ingredient(25, "g", "mozarella"));
+        lesIngredientsdelaRecette.add(new Ingredient(4, "", "oeufs"));
+        lesIngredientsdelaRecette.add(new Ingredient(1, "bouquet", "ciboulette"));
+        lesIngredientsdelaRecette.add(new Ingredient(2, "cuilleres a soupe", "huile"));
+        lesIngredientsdelaRecette.add(new Ingredient(1, "pincée", "sel"));
+        lesIngredientsdelaRecette.add(new Ingredient(1, "pincée", "poivre"));
 
-        livreRecettes.add(new Recette(1,"Omelette ciboulette et mozzarella",10,lesIngredientsdelaRecette,"1) Coupez la mozzarella en 12 morceaux"+"\n"+"2) Cassez les oeufs et fouettez les avec le sel, le poivre et la ciboulette coupée finement"+"\n"+"3) Faire cuire les oeufs sur une poele pendant deux à trois minutes"+"\n"+"4) parsemez les oeufs de mozzarella, couvrez et laissez cuire 7 mn environ, à feu doux, sans y toucher, jusqu’à ce que l’omelette soit juste prise"));
+        livreRecettes.add(new Recette(1, "Omelette ciboulette et mozzarella", 10, lesIngredientsdelaRecette, "1) Coupez la mozzarella en 12 morceaux" + "\n" + "2) Cassez les oeufs et fouettez les avec le sel, le poivre et la ciboulette coupée finement" + "\n" + "3) Faire cuire les oeufs sur une poele pendant deux à trois minutes" + "\n" + "4) parsemez les oeufs de mozzarella, couvrez et laissez cuire 7 mn environ, à feu doux, sans y toucher, jusqu’à ce que l’omelette soit juste prise"));
 
 
-       // Recette recette=matriceRecette[numeroSemaine][numeroJour];
-        Recette recette=livreRecettes.get(0);
-        int tempsDeCuisine=recette.getTempsdecuisine();
+        // Recette recette=matriceRecette[numeroSemaine][numeroJour];
+        Recette recette = livreRecettes.get(0);
+        int tempsDeCuisine = recette.getTempsdecuisine();
         lesIngrédients = recette.getIngredients();
-        String instructions=recette.getInstructions();
-        String Titre=recette.getTitre();
+        String instructions = recette.getInstructions();
+        String Titre = recette.getTitre();
 
-    TextView ViewTempsDeCuisine=findViewById(R.id.tempsCuisine);
-    TextView ViewRecette=findViewById(R.id.Recette);
-    TextView ViewTitre=findViewById(R.id.Titre);
+        TextView ViewTempsDeCuisine = findViewById(R.id.tempsCuisine);
+        TextView ViewRecette = findViewById(R.id.Recette);
+        TextView ViewTitre = findViewById(R.id.Titre);
 
 
-
-     ViewTempsDeCuisine.setText(Integer.toString(tempsDeCuisine));
-    ViewRecette.setText(instructions);
-    ViewTitre.setText(Titre);
-
-        }
-
+        ViewTempsDeCuisine.setText(Integer.toString(tempsDeCuisine));
+        ViewRecette.setText(instructions);
+        ViewTitre.setText(Titre);
 
     }
 
+
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        TextView NbInvites =findViewById(R.id.NbInvites); //on affiche le nombre d'invités choisis
+        TextView NbInvites = findViewById(R.id.NbInvites); //on affiche le nombre d'invités choisis
         NbInvites.setText(Integer.toString(progress));
-        LinearLayout ListeIngrédients=findViewById(R.id.Ingrédients);
+        LinearLayout ListeIngrédients = findViewById(R.id.Ingrédients);
         ListeIngrédients.removeAllViewsInLayout();
-        for (int i=0;i<lesIngrédients.size();i++){
-            Ingredient ingrédient=lesIngrédients.get(i);
+        for (int i = 0; i < lesIngrédients.size(); i++) {
+            Ingredient ingrédient = lesIngrédients.get(i);
             String Quantité;
             Quantité = Integer.toString(ingrédient.getQuantité(progress));
-            String TexteIngrédient=Quantité+" "+ingrédient.getUnite()+" "+ingrédient.getIngredient();
-            TextView tv=new TextView(this);
+            String TexteIngrédient = Quantité + " " + ingrédient.getUnite() + " " + ingrédient.getIngredient();
+            TextView tv = new TextView(this);
             tv.setText(TexteIngrédient);
             ListeIngrédients.addView(tv);
-    }}
+        }
+    }
 
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
@@ -128,7 +128,7 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
     public void onStopTrackingTouch(SeekBar seekBar) {
 
     }
-
+}
     /*void finRecette(View view) {
         // si bouton Fini clique : +1 point !
         if (view.equals(boutonFini)) {
