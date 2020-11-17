82ba14aeec00d48f5c408cabf5f495ad1aeadf6e
Thevenet Hugo <hugo.thevenet777@gmail.com>
2020-11-17 16:28:10 +0100

    recettes effectuees

--- app/src/main/java/com/example/cuistocs/RecettesEffectuees.java
+++ app/src/main/java/com/example/cuistocs/RecettesEffectuees.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +17,7 @@ import java.util.Set;
 public class RecettesEffectuees extends AppCompatActivity {
 //liste les recettes effectuées et permet de les réafficher
 
-
-    SharedPreferences sharedPreferences;
+    
     SharedPreferences.Editor editor;
     LinearLayout carteLayout;
 
@@ -37,9 +37,10 @@ public class RecettesEffectuees extends AppCompatActivity {
         LinearLayout lay = findViewById(R.id.recetteseffectuees);
         lay.removeAllViews();
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         for (int i=0;i<30;i++) {
             String name = "r"+""+i+ ""+"finie";
-            if (sharedPreferences.contains(name)) {
+            if (preferences.contains(name)) {
 
                 Button button = new Button(getApplicationContext());
                 button.setText("Recette" + Integer.toString(i));
