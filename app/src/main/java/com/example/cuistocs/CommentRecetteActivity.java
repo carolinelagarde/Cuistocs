f319edf3920a2336dcddda2ae82b6d8ef58a89cd
Emilie <emirobillard2000@gmail.com>
2020-11-18 19:32:12 +0100

    uploader la photo prise

--- app/src/main/java/com/example/cuistocs/CommentRecetteActivity.java
+++ app/src/main/java/com/example/cuistocs/CommentRecetteActivity.java
@@ -3,6 +3,7 @@ package com.example.cuistocs;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,9 +26,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 import java.util.Vector;
 import android.content.ContentProvider;
 import androidx.core.content.FileProvider;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.firebase.storage.StorageTask.SnapshotBase;
+import java.lang.Object;
 
 public class CommentRecetteActivity extends AppCompatActivity {
 
@@ -50,10 +61,20 @@ public class CommentRecetteActivity extends AppCompatActivity {
 
 
 
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_recette);
+
+
+        //on crée un Firebasestorage qui servira à enregistrer la photo prise et on récupère la référence de stockage
+        storage=FirebaseStorage.getInstance();
+        storageReference=storage.getReference();
+
+
 
 
         Intent deRecetteActivity = getIntent();
@@ -188,17 +209,44 @@ public class CommentRecetteActivity extends AppCompatActivity {
             //création d'un fichier pour la photo
             String time=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //on aura la date précise de la photo
             File photoDir=getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-            try{
-                File photoFile= File.createTempFile("photo"+time,".jpg", photoDir);
+            try {
+                File photoFile = File.createTempFile("photo" + time, ".jpg", photoDir);
                 //on enregistre le chemin complet
-                photoPath=photoFile.getAbsolutePath();
+                photoPath = photoFile.getAbsolutePath();
                 //crée l'URI
-                Uri photoUri= FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider",photoFile);
+                Uri photoUri = FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", photoFile);
                 //transfert uri vers l'intent pour enregistrer la photo dans un fichier temporaire
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                 //on ouvre l'activité par rapport à l'intent
-                startActivityForResult(intent,1);
+                startActivityForResult(intent, 1);
+
+
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setTitle("Uploading...");
+                progressDialog.show();
+
+                StorageReference ref = storageReference.child("images/" + UUID.randomUUID().toString());
+                ref.putFile(Uri.parse(photoPath))
+                        //.addOnSuccessListener(new OnSuccessListener() {
+                            //public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                              //  progressDialog.dismiss();
+                           // }
+                      //  })
+                        .addOnFailureListener(new OnFailureListener() {
+                            public void onFailure(Exception e) {
+                                progressDialog.dismiss();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                                double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot
+                                        .getTotalByteCount());
+                                progressDialog.setMessage("Uploaded" + (int)progress + "%");
+                            }
+                        });
             }
+
+
             catch(IOException e){
                 e.printStackTrace();
             }
@@ -218,10 +266,17 @@ public class CommentRecetteActivity extends AppCompatActivity {
                 Bitmap image= BitmapFactory.decodeFile(photoPath);
                 //afficher l'image
                 imgAffichePhoto.setImageBitmap(image);
+
+                //pour transférer l'image à l'activité où on voit les recettes faites
+               // Intent messageVersAfficherRecettesEffectueesActivity= new Intent(this, AfficherRecettesEffectueesActivity.class);
+                //messageVersAfficherRecettesEffectueesActivity.putExtra("BitmapImage", image);
             }
 
         }
 
 
 
-}
+
+    }
+
+
