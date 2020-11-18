9b739e1c60254b460a790e2089835cb2cb501c55
Thevenet Hugo <hugo.thevenet777@gmail.com>
2020-11-18 18:13:16 +0100

    debug afficher recette encore !

--- app/src/main/java/com/example/cuistocs/RecetteActivity.java
+++ app/src/main/java/com/example/cuistocs/RecetteActivity.java
@@ -42,6 +42,8 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
     int numeroSemaine;
     int numeroRecette;
     String numeroRecetteActuel;
+    int JourAbsoluActuel; //il vaut (numeroJour+1)*(numeroSemaine+1)
+
     Vector<Ingredient> lesIngredients;
 
     SharedPreferences etatBouton;
@@ -74,8 +76,9 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
 
         Intent messagedeChoixJoursActivity = getIntent();
 
-        numeroJour = messagedeChoixJoursActivity.getIntExtra("numero jour", -1); //on recupere l entier nombre de point de l'activité principale
-
+        numeroJour = messagedeChoixJoursActivity.getIntExtra("numero jour", -1);
+        numeroSemaine = messagedeChoixJoursActivity.getIntExtra("numero semaine", -1);
+        JourAbsoluActuel = (numeroJour+1)*(numeroSemaine+1);
 
         SeekBar barre = findViewById(R.id.seekBar); // barre de choix du nombre d'invités
         barre.setOnSeekBarChangeListener(this);
@@ -84,8 +87,8 @@ public class RecetteActivity extends AppCompatActivity implements SeekBar.OnSeek
         //recuperation de la recette
         preferences = getSharedPreferences("lien", Context.MODE_PRIVATE);
 
-        numeroRecetteActuel = preferences.getString(""+numeroJour+"","1");
-        Log.i("NumeroRecette",numeroRecetteActuel);
+        numeroRecetteActuel = preferences.getString(""+JourAbsoluActuel+"","1");
+        Log.i("NumeroRecette",""+JourAbsoluActuel+"");
 
         Menu test= new Menu();
         recette = test.livreRecettes.get(Integer.valueOf(numeroRecetteActuel));
