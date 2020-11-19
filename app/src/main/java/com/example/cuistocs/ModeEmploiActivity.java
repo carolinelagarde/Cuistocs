d96b53913ecb7981e6f8145599045f50d6e3b86b
Josse <josse.muller@student-cs.fr>
2020-11-19 16:30:31 +0100

    mode emploi3

--- app/src/main/java/com/example/cuistocs/ModeEmploiActivity.java
+++ app/src/main/java/com/example/cuistocs/ModeEmploiActivity.java
@@ -27,7 +27,7 @@ public class ModeEmploiActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Mode d'emploi", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Intent versSemaine=new Intent(getApplicationContext(),SemaineActivity.class);
                 startActivity(versSemaine);
