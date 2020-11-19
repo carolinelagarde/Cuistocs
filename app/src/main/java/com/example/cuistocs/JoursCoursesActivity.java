70ea60b86096157764d423133b987ea19f210728
carolinelagarde <caro.lag@outlook.fr>
2020-11-20 00:11:15 +0100

    message d'erreur et bouton bloqué si les jours de course sont pas cohérents

--- app/src/main/java/com/example/cuistocs/JoursCoursesActivity.java
+++ app/src/main/java/com/example/cuistocs/JoursCoursesActivity.java
@@ -6,13 +6,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 //CETTE PAGE PERMET DE SELECTIONNER PLUSIEURS JOURS : ON AFFICHERA ALORS LA LISTE DE COURSE NECESSAIRES DE CES JOURS. ON POURRA ALORS ALLER FAIRE LES COURSES.
 public class JoursCoursesActivity extends AppCompatActivity {
     //définition des premiers et derniers jours
     public int PremierJourSeekbar;
     public int DernierJourSeekbar;
+    public SeekBar barrePremierJour;
 
     //affiche le jour qui correspond à la selection
     public TextView AfficherDernierJourCourses;
@@ -24,20 +28,21 @@ public class JoursCoursesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jours_courses2);
 
+        DernierJourSeekbar=0;
+        PremierJourSeekbar=0;
+
         //récupérer le dernier jour pour lesquels il faut faire les course et l'afficher
         SeekBar barreDernierJour = findViewById(R.id.seekBarDernierJour);
         //récupérer le premier jour pour lesquels il faut faire les course et l'afficher
-        SeekBar barrePremierJour = findViewById(R.id.seekBarPremierJour);
+        barrePremierJour = findViewById(R.id.seekBarPremierJour);
 
         //affichage de la barre
         barrePremierJour.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int PremierJour, boolean b) {
 
-                //prend le numéro du premier jour
-                PremierJourSeekbar=PremierJour;
-
                 //affiche le premier jour à coté de la barre
+                PremierJourSeekbar=PremierJour;
                 AfficherPremierJourCourses = findViewById(R.id.textViewAfficherPremierJour);
                 AfficherPremierJourCourses.setText(Integer.toString(PremierJour));
             }
@@ -46,9 +51,9 @@ public class JoursCoursesActivity extends AppCompatActivity {
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
@@ -56,8 +61,6 @@ public class JoursCoursesActivity extends AppCompatActivity {
         barreDernierJour.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int DernierJour, boolean b) {
-
-                //prend le numero du dernier jour
                 DernierJourSeekbar=DernierJour;
 
                 //affiche le dernier jour à coté de la barre
@@ -69,29 +72,34 @@ public class JoursCoursesActivity extends AppCompatActivity {
             public void onStartTrackingTouch(SeekBar seekBar) {
 
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
         //on aura besoin du nombre d'invités par jour pour faire les courses,
         //pour l'instant on set le nombre d'invités à 1 par défaut via un sharedpreferences
-        SharedPreferences invites= getSharedPreferences("nombreinvites",MODE_PRIVATE);
-        SharedPreferences.Editor editor= invites.edit();
-        for (int i=PremierJourSeekbar;i<=DernierJourSeekbar;i++){
-            editor.putInt("r"+i,1);
+        SharedPreferences invites = getSharedPreferences("nombreinvites", MODE_PRIVATE);
+        SharedPreferences.Editor editor = invites.edit();
+        for (int i = PremierJourSeekbar; i <= DernierJourSeekbar; i++) {
+            editor.putInt("r" + i, 1);
         }
         editor.commit();
     }
 
     //transmettre le premier jour et le dernier jour pour lesquels il faut faire les courses a l'activité liste de courses et passer à cette activité
     public void validerJoursdeCourse(View view) {
-        //le bouton validation permet de passer à l'activité suivante qui affiche les jours voulus
-        Intent versListeDeCourses = new Intent();
-        versListeDeCourses.setClass(this, ListeDeCoursesActivity.class);
-        versListeDeCourses.putExtra("PremierJourCourses",PremierJourSeekbar);
-        versListeDeCourses.putExtra("DernierJourCourses",DernierJourSeekbar);
-        startActivity(versListeDeCourses);
+        if (PremierJourSeekbar>DernierJourSeekbar) {
+            Toast.makeText(getApplicationContext(), "Attention, vos jours ne sont pas cohérents !", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            //le bouton validation permet de passer à l'activité suivante qui affiche les jours voulus
+            Intent versListeDeCourses = new Intent();
+            versListeDeCourses.setClass(this, ListeDeCoursesActivity.class);
+            versListeDeCourses.putExtra("PremierJourCourses", PremierJourSeekbar);
+            versListeDeCourses.putExtra("DernierJourCourses", DernierJourSeekbar);
+            startActivity(versListeDeCourses);
+        }
     }
 }
